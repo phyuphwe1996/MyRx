abbc9e82164664b026400e5eb50162be851518fc
phyu phyu phwe <cumgy.phyuphyuphwe@gmail.com>
2018-05-10 18:49:35 +0630

    add 2nd line

--- MyRx/Program.cs
+++ MyRx/Program.cs
@@ -11,6 +11,8 @@ namespace MyRx
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+
         }
     }
 }
